24b1356b5d2c92dcc80e99338abf25311a24d614
Brandon Rodriguez <rodrigueb6@students.rowan.edu>
2018-06-29 23:47:20 -0400

    server side auth

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import path from 'path';
 import express from 'express';
+import mongoose from 'mongoose';
+import auth from './routes/auth';
+import bodyParser from 'body-parser';
 const app = express();
+app.use(bodyParser.json());
+mongoose.connect('mongodb://localhost/datarank');
 
-app.post('/api/auth', (req, res) => {
-  res.status(400).json({ errors: { global: 'Invalid credentials' } });
-});
+app.use('/api/auth', auth);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
